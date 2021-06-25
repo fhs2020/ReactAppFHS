908951a3303139a90a9e5f428265b967911d74dc
Flavio Henrique de Sousa <flavioh007@gmail.com>
2021-06-25 10:12:12 -0300

    Fecthing data with useEffect

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React, {
+  useState,
+  useEffect
+}from "react";
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+function GitHubUser({login}){
+
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+    .then(res => res.json())
+    .then (setData)
+    .catch(console.error);
+  }, []);
+
+  if(data){
+    return (
+      <div>{JSON.stringify(data)}</div>
+    )
+  }
+  return null;
+}
+ 
+function App(){
+  return <GitHubUser login="fhs2020" />
+
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
